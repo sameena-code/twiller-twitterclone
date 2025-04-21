78b6d868df0fb903958cf70bd520afd71e3c2305
sameena-code <fardeenshailh522@gmail.com>
2025-04-21 20:03:05 +0530

    Update firbase.js

--- twiller/src/context/firbase.js
+++ twiller/src/context/firbase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4QENoyn6__Y-qPD8vqIUE3TlMGbKPWN4",
-  authDomain: "twiller-408b6.firebaseapp.com",
-  projectId: "twiller-408b6",
-  storageBucket: "twiller-408b6.firebasestorage.app",
-  messagingSenderId: "12784604224",
-  appId: "1:12784604224:web:1ced138bfc138261efbba5",
-  measurementId: "G-2S7QRGSQQQ"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId:"",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
